9920ec8d53e99b0a99917f7659b2e199e8a21951
Udnlx <Udnlx@yandex.ru>
2024-07-28 13:56:45 +0300

    добавлен функционал продажи лома

---
+++ site/templates/prodaja_lom.php
@@ -0,0 +1,188 @@
+<?php namespace ProcessWire;
+
+$main_options = $pages->get('template=main_options');
+$main_price_gold = $main_options->main_price_gold;
+$main_price_silver = $main_options->main_price_silver;
+$main_price_platinum = $main_options->main_price_platinum;
+$main_price_palladium = $main_options->main_price_palladium;
+
+if(isset($_SESSION['operator'])){
+    $operator = $_SESSION['operator'];
+} else {
+    $operator = 'no_operator';
+}
+
+if(isset($_SESSION['point'])){
+    $selected_point = $_SESSION['point'];
+} else {
+    $selected_point = 'no_point';
+}
+
+if(isset($_SESSION['id_point'])){
+    $selected_id_point = $_SESSION['id_point'];
+} else {
+    $selected_id_point = 'no_id_point';
+}
+
+$access = '';
+if(isset($_SESSION['access'])){
+    $access = $_SESSION['access'];
+}
+
+if ($operator == 'no_operator' || $selected_point == 'no_point') {
+?>
+    <div id="content" style="max-width: 700px;">
+    	<h1 class="uk-heading-hero uk-text-center">Продажа лома</h1>
+        <div class="uk-card uk-card-default uk-card-body uk-width-1-1 uk-flex uk-flex-column">
+            <h3 class="uk-card-title">Потеряна сессия или точка, перезайти</h3>
+            <a class="uk-margin-small uk-button uk-button-default" href="/login/">Перезайти</a>
+        </div>
+    </div>
+<?php    
+} else {
+
+//Формирование таблицы с остатками
+$remain_tables_startday = '';
+$startday = $pages->get('id_point=' . $selected_id_point . '_startday');
+$actual = $pages->get('id_point=' . $selected_id_point . '_actual');
+$reserv = $pages->get('id_point=' . $selected_id_point . '_reserv');
+
+if ($startday != '' || $actual != '' || $reserv != '') {
+$actual_date = $startday->actual_date;
+include 'remains_table_archive.php';
+$remain_tables_startday .= '<h4 class="uk-card-title uk-margin-remove">Дата таблиц: ' . $actual_date . '</h4><hr>';
+}
+
+if ($startday == '' || $actual == '' || $reserv == '') {
+    $remain_tables_startday .= '
+    <h2 class="uk-margin-remove uk-card-title" style="color:red;font-weight:700;text-align:center;">Произошла ошибка получения остатков!<br>Пожалуйста обратитесь к разработчику!</h2>
+    ';
+} else {
+    include 'remains_table.php';
+}
+
+?>
+
+<div id="content">
+	<h1 class="uk-margin-remove uk-heading-hero uk-text-center">Продажа лома</h1>
+	<div>
+
+        <div>
+            <div class="pagemenu uk-width-1-1 uk-flex">
+                <a class="menu-link" href="/">На главную</a>
+                <a class="menu-link" href="/prodazha-tip-prodazhi/">Выбрать другой тип продажи</a>
+            </div>
+        </div>
+
+        <div>
+            <div class="uk-card uk-card-default uk-card-body uk-flex uk-flex-column">
+                <form class="uk-flex uk-flex-column" id="select_seat" action="/prodazha-lom-registratciia/" method="post">
+                    <div class="uk-margin-small-top uk-hidden">
+                        <input class="uk-input" id="selected_date" type="text" name="selected_date" value="<?php echo $today; ?>">
+                    </div>
+                    <div class="uk-margin-small-top uk-hidden">
+                        <input class="uk-input" id="selected_point" type="text" name="selected_point" value="<?php echo $selected_point; ?>">
+                    </div>
+                    <div class="uk-margin-small-top uk-hidden">
+                        <input class="uk-input" id="selected_idpoint" type="text" name="selected_idpoint" value="<?php echo $selected_id_point; ?>">
+                    </div>
+                    <div class="uk-margin-small-top uk-hidden">
+                        <input class="uk-input" id="selected_worker" type="text" name="selected_worker" value="<?php echo $operator; ?>">
+                    </div>
+
+                    <div class="uk-margin-small-top uk-hidden">
+                        <input class="uk-input" id="main_price_gold" type="text" name="main_price_gold" value="<?php echo $main_price_gold; ?>">
+                    </div>
+                    <div class="uk-margin-small-top uk-hidden">
+                        <input class="uk-input" id="main_price_silver" type="text" name="main_price_silver" value="<?php echo $main_price_silver; ?>">
+                    </div>
+                    <div class="uk-margin-small-top uk-hidden">
+                        <input class="uk-input" id="main_price_platinum" type="text" name="main_price_platinum" value="<?php echo $main_price_platinum; ?>">
+                    </div>
+                    <div class="uk-margin-small-top uk-hidden">
+                        <input class="uk-input" id="main_price_palladium" type="text" name="main_price_palladium" value="<?php echo $main_price_palladium; ?>">
+                    </div>
+
+                    <div class="uk-margin-small-top">
+                        <label for="selected_proba">Выберите пробу</label>
+                        <select class="uk-select" id="selected_proba" name="selected_proba">
+                            <option>375</option>
+                            <option>333</option>
+                            <option>417</option>
+                            <option>500</option>
+                            <option>585</option>
+                            <option>620</option>
+                            <option>750</option>
+                            <option>800</option>
+                            <option>850</option>
+                            <option>875</option>
+                            <option>900</option>
+                            <option>916</option>
+                            <option>958</option>
+                            <option>990</option>
+                            <option>999.9</option>
+                            <option>Ag</option>
+                            <option>Pt</option>
+                            <option>Pd</option>
+                        </select>
+                    </div>
+                    <div class="uk-margin-small-top">
+                        <input class="uk-input custom1" id="selected_weight" type="text" name="selected_weight" value="" placeholder="Вес" autocomplete="off" required>
+                    </div>
+                    <div class="uk-margin-small-top">
+                        <label for="price_gramm">Цена за грамм</label>
+                        <input class="uk-input readonly" id="price_gramm" type="text" name="price_gramm" value="<?php echo round(($main_price_gold/585)*375, 2); ?>" autocomplete="off" required>
+                    </div>
+                    <div class="uk-margin-small-top">
+                        <label for="selected_price">Стоимость</label>
+                        <input class="uk-input readonly" id="selected_price" type="text" name="selected_price" value="0.00" autocomplete="off" required>
+                    </div>
+                    <div class="uk-margin-small-top">
+                        <input class="uk-input" id="selected_pay" type="text" name="selected_pay" value="" placeholder="Сколько отдали" autocomplete="off" required>
+                    </div>
+                    <div class="uk-margin-small-top">
+                        <label for="cash_card">Вид платежа</label>
+                        <select class="uk-select" id="cash_card" name="cash_card">
+                            <option>Наличный расчет</option>
+                            <option>Безналичный расчет</option>
+                        </select>
+                    </div>
+                    <div class="uk-margin-small-top">
+                        <label for="selected_proba">Квитанция</label>
+                        <select class="uk-select" id="selected_paytype" name="selected_paytype">
+                            <option>Нет</option>
+                            <option>Да</option>
+                        </select>
+                    </div>
+
+                    <div id="data_client" class="uk-hidden">
+                        <div class="uk-margin-small-top">
+                            <input class="uk-input" id="client_name" type="text" name="client_name" value="" placeholder="ФИО клиента" autocomplete="off">
+                        </div>
+                        <div class="uk-margin-small-top">
+                            <input class="uk-input" id="client_passport" type="text" name="client_passport" value="" placeholder="Паспорт клиента" autocomplete="off">
+                        </div>
+                        <div class="uk-margin-small-top">
+                            <input class="uk-input" id="client_address" type="text" name="client_address" value="" placeholder="Адрес клиента" autocomplete="off">
+                        </div>
+                    </div>
+                    
+                    <div class="uk-margin-small-top uk-flex uk-flex-column">
+                        <button class="uk-margin-small-top uk-button uk-button-default" type="submit">Зарегистрировать</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        
+        <div>
+            <div class="uk-card uk-card-default uk-card-body uk-flex uk-flex-column">
+                <?php echo $remain_tables_startday; ?>
+            </div>
+        </div>
+        
+    </div>
+</div>
+
+<?php   
+}
+?>
