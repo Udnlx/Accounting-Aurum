eae748f35fa7be0fe55ff8c67ea789ec2ccb78b3
Udnlx <Udnlx@yandex.ru>
2024-12-23 12:39:41 +0300

    скрипт добавление долга со страницы проверки заявки

--- add_new_arrear.php
+++ add_new_arrear.php
@@ -19,26 +19,27 @@ $arrear_descript = $_POST['arrear_descript'];
 if ($arrear_date == '' || $arrear_point == '' || $arrear_idpoint == '' || $arrear_worker == '' || $arrear_person == '' || $arrear_sum == '' || $arrear_descript == '') {
     echo '<p id="result_arrear_add" class="messages" style="color: red;">Ошибка. Долг не зарегестрирован.<br>Проверьте заполненность полей и повторите попытку.</p>';    
 } else {
-	$operation_id = '00000';
-	// //Регестрируем запись
-	// $pages->add('affinaj_itm', 1266 , [
-    // 'title' => date("Y-m-d H:i") . ' Аффинаж - Расход - ' . $point . '',
-    // 'product_status' => 'Открыт',
-    // 'type_operation' => 'Аффинаж',
-    // 'undertype_operation' => 'Расход',
-    // 'date' => $date,
-    // 'point' => $point,
-    // 'id_point' => $idpoint,
-    // 'worker' => $worker,
-    // ]);
-    // $operation_page = $pages->get('title=' . date("Y-m-d H:i") . ' Аффинаж - Расход - ' . $point . '');
-    // $operation_id = $operation_page->id;
+	//$operation_id = '00000';
+	//Регестрируем запись
+	$pages->add('arrears_itm', 1872 , [
+    'title' => date("Y-m-d H:i") . ' Долг - ' . $arrear_person . ' - ' . $arrear_sum . '',
+    'product_status' => 'Открыт',
+    'date' => $arrear_date,
+    'point' => $arrear_point,
+    'id_point' => $arrear_idpoint,
+    'worker' => $arrear_worker,
+    'client_name' => $arrear_person,
+    'sum' => $arrear_sum,
+    'description_operation' => $arrear_descript,
+    ]);
+    $operation_page = $pages->get('title=' . date("Y-m-d H:i") . ' Долг - ' . $arrear_person . ' - ' . $arrear_sum . '');
+    $operation_id = $operation_page->id;
 
-	// //Записываем регистрацию в лог
-	// $log = '';
-    // $log .= date("Y-m-d H:i") . ' Аффинаж - Расход - ' . $point . ' === ';
-    // $log .= 'Запись занесена: ' . $worker . ', ID записи: ' . $operation_id; 
-    // file_put_contents(__DIR__ . '/site/templates/log_affinaj.txt', $log . PHP_EOL, FILE_APPEND);
+	//Записываем регистрацию в лог
+	$log = '';
+    $log .= date("Y-m-d H:i") . ' Долг - ' . $arrear_person . ' - ' . $arrear_sum . ' === ';
+    $log .= 'Запись занесена: ' . $arrear_worker . ', ID записи: ' . $operation_id . ', Описание долга: ' . $arrear_descript; 
+    file_put_contents(__DIR__ . '/site/templates/log_arrears.txt', $log . PHP_EOL, FILE_APPEND);
 
 	//Сообщение для пользователя
 	//echo $arrear_date . $arrear_point . $arrear_idpoint . $arrear_worker . $arrear_person . $arrear_sum . $arrear_descript;
