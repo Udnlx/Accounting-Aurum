c959a3d16605ed4dae10eb5ebe61cad6d0224d36
Udnlx <Udnlx@yandex.ru>
2024-12-28 11:15:19 +0300

    добавленна, но не активированна и не настроена функция логаута

--- site/templates/scripts/main.js
+++ site/templates/scripts/main.js
@@ -567,3 +567,29 @@ $('#reg_new_arrear').click(function() {
 	}
 });
 //Закрытие аффинажа
+
+
+
+// //Логаут при бездействии
+// var inactivityTime = function () {
+//     var time;
+//     window.onload = resetTimer;
+//     // DOM Events
+//     document.onmousemove = resetTimer;
+//     document.onkeydown = resetTimer;
+
+//     function logout() {
+//         alert("You are now logged out.")
+//         //location.href = 'logout.html'
+//     }
+
+//     function resetTimer() {
+//         clearTimeout(time);
+//         time = setTimeout(logout, 3000)
+//         // 1000 milliseconds = 1 second
+//     }
+// };
+
+// window.onload = function() {
+//   	inactivityTime();
+// }
